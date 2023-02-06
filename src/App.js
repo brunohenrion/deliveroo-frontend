0ce9450a42cc88e9c023d53f39bd673cf9d2ba8a
HENRION <nono@mbp-de-henrion.lan>
2023-02-06 15:04:19 +0100

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import logo from "./assets/images/logo-teal.svg";
 
 function App() {
-  return (
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      "https://site--deliveroo-backend--rjk8ksgk7kql.code.run/"
+    );
+    //console.log(response.data);
+    setData(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={logo} alt="" />
       </header>
+      <section>
+        <div>
+          <div className="description">
+            <h2>{data.restaurant.name}</h2>
+            <p>{data.restaurant.description}</p>
+          </div>
+          <div>
+            <img src={data.restaurant.picture} alt="" />
+          </div>
+        </div>
+      </section>
+
+      <section className="menu"></section>
     </div>
   );
 }
